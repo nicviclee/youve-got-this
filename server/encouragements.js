5bd4fa62bd162ada24e52470dbe19baef3d0cf20
Mike Fink <mike.g.fink@gmail.com>
2016-05-06 12:47:37 -0700

    Adds quote lists in array form temporarily.

---
+++ server/encouragements.js
@@ -0,0 +1,38 @@
+/* encouragements.js
+ Includes quotes from Alissa Marquess http://creativewithkids.com/64-encouraging-things-to-say-to-kids/
+ and http://www.usingenglish.com/files/pdf/classroom-language-phraselist-for-praising-consoling-and-encouraging.pdf
+ as well as quotes from Nicole Lee and Mike Fink.
+ */
+var encouragements = [
+    "You've got this!",
+    "Keep going",
+    "Don't give up",
+    "You can do it!",
+    "You’re nearly there!",
+    "You are loved.",
+    "You make me smile",
+    "I have faith in you.",
+    "I know you can handle it.",
+    "You are creative.",
+    "Trust your instincts.",
+    "Your ideas are worthwhile.",
+    "You are capable.",
+    "You are deserving.",
+    "You are strong.",
+    "You make a difference.",
+    "You can learn from your mistakes.",
+    "You can ask for help.",
+    "You are learning.",
+    "I believe in you.",
+    "You are valuable.",
+    "You are interesting.",
+    "You are beautiful.",
+    "You are important.",
+    "Your ideas matter.",
+    "You are able to do work that matters.",
+    "Your ideas are interesting.",
+    "You are a fun person.",
+    "You make me smile.",
+    "I love you."
+];
+export default encouragements;
