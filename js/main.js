0988f4058e4a19f0b36af3b012a8a3827362b1ae
Mike Fink <mike.g.fink@gmail.com>
2016-05-05 17:43:28 -0700

    Creates calls AJAX calls to server when subscribe/unsubscribing. Uses jquery for AJAX calls and DOM manipulation.

--- js/main.js
+++ js/main.js
@@ -1,49 +1,151 @@
 'use strict';
 
 var reg; //ServiceWorkerRegistration object
-var sub; //subscription
 var isSubscribed = false;
-var subscribeButton = document.querySelector('button');
+var subscribeButton;  // document.querySelector('button');
+var serverUrl = "http://placeholder.com/";
 
-if ('serviceWorker' in navigator) {
- console.log('Service Worker is supported');
- navigator.serviceWorker.register('sw.js').then(function() {
-   return navigator.serviceWorker.ready;
- }).then(function(serviceWorkerRegistration) {
-     reg = serviceWorkerRegistration;
-     subscribeButton.disabled = false;
-     console.log('Service Worker is ready :^)', reg);
- }).catch(function(err) {
-   console.log('Service Worker Error :^(', err);
- });
-}
-
-subscribeButton.addEventListener('click', function() {
-    if (isSubscribed) {
-        unsubscribe();
+$(document).ready(function() {
+    subscribeButton = $('#subscribeButton');
+    console.log(subscribeButton);
+    if ('serviceWorker' in navigator) {
+        console.log('Service Worker is supported');
+        navigator.serviceWorker.register('sw.js').then(function() {
+            return navigator.serviceWorker.ready;
+        }).then(function(serviceWorkerRegistration) {
+            reg = serviceWorkerRegistration;
+            subscribeButton.prop('disabled', false);
+            console.log('Service Worker is ready :^)', reg);
+            initialiseState();
+        }).catch(function(err) {
+            console.log('Service Worker Error :^(', err);
+        });
     } else {
-        subscribe();
+        console.warn("Service workers aren't supported in this browser.");
     }
+
+    subscribeButton.click(function() {
+        console.log('Subscribe button clicked.');
+        if (isSubscribed) {
+            unsubscribe();
+        } else {
+            subscribe();
+        }
+    });
 });
+
+function initialiseState() {
+  // Are Notifications supported in the service worker?
+  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+    console.warn('Notifications aren\'t supported.');
+    return;
+  }
+
+  // Check the current Notification permission.
+  // If its denied, it's a permanent block until the
+  // user changes the permission
+  if (Notification.permission === 'denied') {
+    console.warn('The user has blocked notifications.');
+    return;
+  }
+
+  // Check if push messaging is supported
+  if (!('PushManager' in window)) {
+    console.warn('Push messaging isn\'t supported.');
+    return;
+  }
+
+  // We need the service worker registration to check for a subscription
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    // Do we already have a push message subscription?
+    serviceWorkerRegistration.pushManager.getSubscription()
+      .then(function(subscription) {
+        // Enable any UI which subscribes / unsubscribes from
+        // push messages.
+        subscribeButton.prop('disabled', false); false;
+
+        if (!subscription) {
+          // We aren't subscribed to push, so set UI
+          // to allow the user to enable push
+          return;
+        }
+
+        // Keep your server in sync with the latest subscriptionId
+        sendSubscriptionToServer(subscription);
+
+        // Set your UI to show they have subscribed for
+        // push messages
+        subscribeButton.text('Unsubscribe');
+        isSubscribed = true;
+      })
+      .catch(function(err) {
+        console.warn('Error during getSubscription()', err);
+      });
+  });
+}
 
 //Note: client gets new reg ID everytime they re-subscribe
 function subscribe() { //subscribe to push messaging
-    reg.pushManager.subscribe({userVisibleOnly: true}).
-    then(function(pushSubscription) {
-        sub = pushSubscription;
-        console.log('Subscribed! Endpoint:', sub.endpoint);
-        subscribeButton.textContent = 'Unsubscribe';
+    console.log('Subscribing.');
+    subscribeButton.prop('disabled', true);
+    reg.pushManager.subscribe({userVisibleOnly: true})
+    .then(function(pushSubscription) {
+        console.log('Subscribed to Notification Server! Endpoint:', pushSubscription.endpoint);
+        subscribeButton.prop('disabled', false);
+        subscribeButton.text('Unsubscribe');
         isSubscribed = true;
+        sendSubscriptionToServer(pushSubscription);
+    }).catch(function(error) {
+        // During development it often helps to log errors to the
+        // console. In a production environment it might make sense to
+        // also report information about errors back to the
+        // application server.
+        console.log('Error subscribing.', error);
     });
 }
 
 function unsubscribe() {
-    sub.unsubscribe().then(function(event) {
-        subscribeButton.textContent = 'Subscribe';
-        console.log('Unsubscribed!', event);
-        isSubscribed = false;
+    subscribeButton.prop('disabled', true);
+    reg.pushManager.getSubscription().then(function(pushSubscription) {
+        if (!pushSubscription) {
+            isSubscribed = false;
+            subscribeButton.prop('disabled', false);
+            subscribeButton.text('Subscribe');
+            return;
+        }
+
+        pushSubscription.unsubscribe().then(function(event) {
+            subscribeButton.prop('disabled', false);
+            subscribeButton.text('Subscribe');
+            console.log('Unsubscribed!', event);
+            isSubscribed = false;
+            removeSubscriptionFromServer(pushSubscription);
+        }).catch(function(err) {
+            console.log('Error unsubscribing.', err);
+            subscribeButton.textConent = 'Subscribe';
+        });
     }).catch(function(err) {
-        console.log('Error unsubscribing', err);
-        subscribeButton.textConent = 'Subscribe';
+        console.error('Error thrown while getting subscription to unsubscribe.', err);
     });
+}
+
+function sendSubscriptionToServer(subscription) {
+    postToAppServer('register', function(data) {
+        // Registration successful
+        console.log('Registered with Application Server! Data:', data);
+    });
+}
+
+function removeSubscriptionFromServer(subscription) {
+    postToAppServer('unregister', subscription.endpoint, function(data) {
+        // Registration successful
+        console.log('Unregistered with Application Server! Data:', data);
+    });
+}
+
+function postToAppServer(action, endpoint, callback) {
+    $.post(serverUrl + action, endpoint, function(data) {
+        // Do something
+        callback(data);
+    }, 'JSON');
 }
